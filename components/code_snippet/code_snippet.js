c8310f5425c3a37bb3c5b72b16cd377b58e2f948
Kevin Powell <kevin@eightshapes.com>
2017-10-25 23:00:24 -0500

    Scss version of mixin working

--- components/code_snippet/code_snippet.js
+++ components/code_snippet/code_snippet.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
 
     $("#style-preprocessor-less, #style-preprocessor-stylus").hide();
     $("[name='style-preprocessor']").on("change", showMixinTab);
+
+
+    var clipboard = new Clipboard('.es-code-snippet__copy-button');
+    clipboard.on('success', function(e) {
+        $(e.trigger).removeClass("es-code-snippet__copy-button--clicked");
+        $(e.trigger).prop('offsetHeight');
+        $(e.trigger).addClass("es-code-snippet__copy-button--clicked").find(".es-code-snippet__copy-response").text('Copied!');
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        $(e.trigger).removeClass("es-code-snippet__copy-button--clicked");
+        $(e.trigger).prop('offsetHeight');
+        $(e.trigger).addClass("es-code-snippet__copy-button--clicked").find(".es-code-snippet__copy-response").text('Press Ctrl + C to copy');
+    });
+
+    $("body").on("click", ".es-code-snippet__copy-button", function(e){
+        e.preventDefault();
+    });
 });
